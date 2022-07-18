2a3a2454c7a2830361d2e09566b9dddd3ddc12c8
SangueSpada <kankilic.1888465@studenti.uniroma1.it>
2022-07-18 17:05:16 +0200

    fixing meteo update

--- src/test/test.js
+++ src/test/test.js
@@ -1,5 +1,6 @@
 
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname,'../.env')});
 process.env.TEST=true;
 const chai = require('chai');
 var expect = chai.expect;
